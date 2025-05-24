c1b95e430e1517385f2fd356a66cf0ba6c08bec8
amanverma-765 <akverma4aman@gmail.com>
2025-05-24 16:05:59 +0530

    update provider url

--- cassini/src/main/java/com/ark/cassini/utils/AppConstants.kt
+++ cassini/src/main/java/com/ark/cassini/utils/AppConstants.kt
@@ -1,7 +1,7 @@
 package com.ark.cassini.utils
 
 object AppConstants {
-    const val PROVIDER_URL = "https://raw.githubusercontent.com/amanverma-765/modflix-providers/main/modflix.json"
+    const val PROVIDER_URL = "https://raw.githubusercontent.com/amanverma-765/modflix/refs/heads/android/providers/modflix.json"
     const val IMDB_BASE_URL_1 = "https://cinemeta-live.strem.io/meta"
     const val IMDB_BASE_URL_2 = "https://v3-cinemeta.strem.io/meta"
     const val TMDB_CHARACTERS_BASE_URL = "https://image.tmdb.org"
